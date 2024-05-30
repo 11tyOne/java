d122e910f6d84f6841b99a5534e98047a714681b
alex <alkuro14@gmail.com>
2024-05-30 23:29:17 +0530

    second commit - print vs println

--- first/src/Hello.java
+++ first/src/Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-      System.out.println("helo");
+      System.out.print("helo ");
+      System.out.println("world");
+      System.out.println("!!");
     }
   }
