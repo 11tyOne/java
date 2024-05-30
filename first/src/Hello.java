216c29925f07a39d009aeef2b3bef18943388025
alex <alkuro14@gmail.com>
2024-05-30 23:44:48 +0530

    third commit - comments

--- first/src/Hello.java
+++ first/src/Hello.java
@@ -1,5 +1,14 @@
+/**
+ * javadoc comments (extra asterk in the start)
+ * used for api documentation and usually goes before all methods and classes
+ */
+
 public class Hello {
-    public static void main(String[] args) {
+    public static void main(String[] args) {  // single line comment
+      /*
+       * main() is a method
+       * multi line comment
+       */
       System.out.print("helo ");
       System.out.println("world");
       System.out.println("!!");
